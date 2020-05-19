dd72a443a89287304f4775ce782cf7b11ecf563f
sehoon123 <sehoon0519@naver.com>
2020-05-19 13:54:30 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+    public static int sum(int a, int b) {
+        return a + b;
+    }
+    public static int minus(int a, int b) {
+        return a - b;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(sum(1,2));
+        System.out.println(minus(1,2));
+    }
+}
